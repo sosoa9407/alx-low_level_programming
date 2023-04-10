32f3ef38836a820c4446a93db2196e847a974de8
sosoa9407 <sosoa9407@gmail.com>
2023-04-10 13:38:10 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  * Return: always 0
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int sum = 0;
 	char *c;
@@ -18,7 +18,7 @@ int main (int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-			if(*c < '0' || *c > '9')
+			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
