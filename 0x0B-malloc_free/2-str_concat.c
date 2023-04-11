ed8d112cbd18541ecf4566c6f516293acdb105b6
sosoa9407 <sosoa9407@gmail.com>
2023-04-11 14:11:07 -0700

    f

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,8 +35,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = '\0';
 
-	size1 = _strln(s1);
-	size2 = _strln(s2);
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (m == 0)
