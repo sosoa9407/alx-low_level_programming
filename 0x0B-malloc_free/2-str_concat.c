7ca7a67e7d6440822090e60a6fe6c7050b61f0b5
sosoa9407 <sosoa9407@gmail.com>
2023-04-11 14:09:11 -0700

    f

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
+
+/**
  * *str_concat -  function that concatenates two strings
  *
- * @s1: string
- * @s2: string
+ * @s1: string 1
+ * @s2: string 2
  *
- * Return: int
+ * Return: pointer
  *
  */
 
